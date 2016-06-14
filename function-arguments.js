3171757622bbf3d617d09aae692436843cb98031
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-14 05:44:02 -0400

    .

---
+++ function-arguments.js
@@ -0,0 +1,13 @@
+function math(){
+
+}
+console.log(function math (53));
+console.log(function math (61));
+console.log(function math (67));
+
+//
+
+function math(x, y, z){
+  return 53, 61, 67;
+}
+console.log(function math();
